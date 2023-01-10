48e3c160eaf406417e45d9f7e87e4f14e88ef50d
fseasy <readonlyfile@hotmail.com>
2023-01-10 18:56:02 +0800

    finish 2022-year-end-review

--- local_serve.sh
+++ local_serve.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+
+if [ "$1" = "full" ]; then
+    inc_args=""
+else
+    inc_args="--incremental"
+fi
+
+if [ ! -e "assets/bef" ] && [ -e "../blog-extra-file" ]; then
+    echo "Link blog-extra-file => assets/bef"
+    cd assets 
+    ln -s ../../blog-extra-file bef
+    cd ..
+fi
+
 echo "PLEASE clone blog-extra-file to root and rename to bef"
-bundle exec jekyll serve -w --host 127.0.0.1
+bundle exec jekyll serve -w "$inc_args" --host 127.0.0.1
