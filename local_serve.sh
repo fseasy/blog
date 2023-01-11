746edc7bd155e137443bf2a41e8ed0d09ea2adc3
fseasy <readonlyfile@hotmail.com>
2023-01-11 12:20:47 +0800

    remove useless bef link

--- local_serve.sh
+++ local_serve.sh
@@ -6,12 +6,5 @@ else
     inc_args="--incremental"
 fi
 
-if [ ! -e "assets/bef" ] && [ -e "../blog-extra-file" ]; then
-    echo "Link blog-extra-file => assets/bef"
-    cd assets 
-    ln -s ../../blog-extra-file bef
-    cd ..
-fi
-
 echo "PLEASE clone blog-extra-file to root and rename to bef"
 bundle exec jekyll serve -w "$inc_args" --host 127.0.0.1
