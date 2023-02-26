95604914a274cd7a317d00e6efa0c6daa636c9ff
fseasy <readonlyfile@hotmail.com>
2023-02-26 17:26:08 +0800

    re-write side-nav, toc setting. now it looks better

---
+++ _includes/extra_fn/dependency_3rd/utils.js
@@ -0,0 +1,18 @@
+{% unless loadUtilsDone %}
+
+// wait for final. copy from https://stackoverflow.com/a/4541963/4869018
+var waitForFinalEvent = (function () {
+    var timers = {};
+    return function (callback, ms, uniqueId) {
+      if (!uniqueId) {
+        uniqueId = "Don't call this twice without a uniqueId";
+      }
+      if (timers[uniqueId]) {
+        clearTimeout (timers[uniqueId]);
+      }
+      timers[uniqueId] = setTimeout(callback, ms);
+    };
+})();
+
+{% assign loadUtilsDone = true %}
+{% endunless %}
